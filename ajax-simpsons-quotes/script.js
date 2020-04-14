9b8bfa77634f7deb509e47cecf4a63db5db46789
Leslie <lesliec38@hotmail.fr>
2020-04-14 10:31:00 +0200

    first commit

---
+++ ajax-simpsons-quotes/script.js
@@ -0,0 +1,23 @@
+// This function loads pokemon data from the Pokemon API
+function fetchSimpsonsJSON() {
+    // Feel free to download this HTML and edit it, to use another Pokemon ID
+    const simpsonsId = 1;
+    const url = `https://simpsons-quotes-api.herokuapp.com/quotes`;
+    axios.get(url)
+      .then(function(response) {
+        return response.data[0]; // response.data instead of response.json() with fetch
+      })
+      .then(function(simpsons) {
+        console.log('data decoded from JSON:', simpsons);
+  
+        // Build a block of HTML
+        const simpsonsHtml = `
+          <p><strong>${simpsons.character}</strong></p>
+          <p>${simpsons.quote}</p>
+          <img src="${simpsons.image}" />
+        `;
+        document.querySelector('#simpsons').innerHTML = simpsonsHtml;
+      });
+  }
+  
+  fetchSimpsonsJSON();
